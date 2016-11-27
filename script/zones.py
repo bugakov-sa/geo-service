b83183c64d8270c0ec77879a4b1e42c7f9886188
user <bugakov.sa.88@gmail.com>
2016-11-27 17:39:18 +0300

    fix

--- script/zones.py
+++ script/zones.py
@@ -3,4 +3,4 @@ delimiter = ','
 with open('..\data\zones.csv', 'w') as file:
     for lon in xrange(-180, 180):
         for lat in xrange(-89, 90):
-            file.write(str(lat) + delimiter + str(lon) + delimiter + str(1000000) + '\n')
+            file.write(str(lat) + delimiter + str(lon) + delimiter + str(111197) + '\n')
