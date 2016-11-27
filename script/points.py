62872b1c4493bc4940ed4eb6d413c9138bb7749b
user <bugakov.sa.88@gmail.com>
2016-11-27 10:49:19 +0300

    fix

--- script/points.py
+++ script/points.py
@@ -5,5 +5,5 @@ delimiter = ','
 with open('..\data\points.csv', 'w') as file:
     for i in xrange(0, pointsAmount):
         file.write(
-            str(math.degrees(math.asin(random.uniform(0, 1))) * (-1 ** (i % 2))) + delimiter +
+            str(math.degrees(math.asin(random.uniform(0, 1))) * ((-1) ** (i % 2))) + delimiter +
             str(random.uniform(-180, 180)) + '\n')
--- script/users.py
+++ script/users.py
@@ -5,5 +5,5 @@ delimiter = ','
 with open('..\data\users.csv', 'w') as file:
     for userId in xrange(0, usersAmount):
         file.write(str(userId) + delimiter +
-                   str(math.degrees(math.asin(random.uniform(0, 1))) * (-1 ** (userId % 2))) + delimiter +
+                   str(math.degrees(math.asin(random.uniform(0, 1))) * ((-1) ** (userId % 2))) + delimiter +
                    str(random.uniform(-180, 180)) + '\n')
