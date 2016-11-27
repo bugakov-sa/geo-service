d5995ec2266e66074f764b38e90a114cab404d76
user <bugakov.sa.88@gmail.com>
2016-11-27 11:30:16 +0300

    sphereDist

--- src/main/scala-2.11/testtask/service/TestLocationService.scala
+++ src/main/scala-2.11/testtask/service/TestLocationService.scala
@@ -4,6 +4,8 @@ import testtask.dao.UsersDao
 import testtask.entity.{Point, ZoneKey}
 import testtask.ref.ZonesRef
 
+import math._
+
 class TestLocationService(usersDao: UsersDao, zonesRef: ZonesRef) {
   def test(userId: Long, point: Point) = {
     val userPoint = usersDao.select(userId)
@@ -14,7 +16,8 @@ class TestLocationService(usersDao: UsersDao, zonesRef: ZonesRef) {
       Some(sphereDist(userPoint.get, point) <= zoneData.get.distanceError)
   }
 
-  private def sphereDist(p1: Point, p2: Point) = {
-    0.0
-  }
+  private val EARTH_RADIUS = 6371000
+
+  private def sphereDist(p1: Point, p2: Point) =
+    EARTH_RADIUS * acos(sin(p1.lat) * sin(p2.lat) + cos(p1.lat) * cos(p2.lat) * cos(p1.lon - p2.lon))
 }
