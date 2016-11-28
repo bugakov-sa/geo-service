84b5f09b41b1f1534a9b3ff2b5853fe7a3c6d5be
user <bugakov.sa.88@gmail.com>
2016-11-28 12:08:10 +0300

    fix

--- src/main/scala-2.11/testtask/service/ZonesStatService.scala
+++ src/main/scala-2.11/testtask/service/ZonesStatService.scala
@@ -11,11 +11,13 @@ class ZonesStatService(zones: ZonesRef) extends Consumer[UserEvent] {
   private val stat = zones.keys.map(k => (k, new AtomicLong(0))).toMap
 
   override def accept(event: UserEvent): Unit = event match {
-    case UserUpdatedEvent(userId, oldPoint, newPoint) =>
-      if (oldPoint.isDefined)
-        stat.get(zoneKey(oldPoint.get)).foreach(_.decrementAndGet())
+    case UserUpdatedEvent(_, oldPoint, newPoint) =>
+      oldPoint.foreach(
+        point => stat.get(zoneKey(point))
+          .foreach(_.decrementAndGet())
+      )
       stat.get(zoneKey(newPoint)).foreach(_.incrementAndGet())
-    case UserDeletedEvent(userId, point) =>
+    case UserDeletedEvent(_, point) =>
       stat.get(zoneKey(point)).foreach(_.decrementAndGet())
   }
 
